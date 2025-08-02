bcfa278c8b1e40075477decf0af90ba7d6d6852c
Aashi Jain <aashijain064@gmail.com>
2025-08-02 22:37:44 +0530

    Add files via upload

---
+++ RotatedSortedArraySearch.java
@@ -0,0 +1,27 @@
+public class RotatedSortedArraySearch {
+  int search(int[] arr, int key) {
+       int n = arr.length;
+       int low =0;
+       int high = n-1;
+       while(low<=high)
+       {
+           int mid = (low + high)/2;
+           if(arr[mid]==key)
+           return mid;
+           if(arr[mid]>=arr[low])
+           {
+               if(arr[low]<=key && arr[mid]>=key)
+               high = mid-1;
+               else
+               low = mid+1;
+           }
+           else
+           {
+               if(arr[mid]<=key && arr[high]>=key)
+               low = mid +1;
+               else
+               high = mid -1;
+           }
+       }
+       return -1; 
+}
