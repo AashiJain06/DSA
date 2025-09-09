0690e0069c5306699cf55ba4c658a61047d4f95f
Aashi Jain <aashijain064@gmail.com>
2025-09-09 23:16:31 +0530

    Add files via upload

---
+++ AssignMicesHole.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class AssignMicesHole {
+  public int assignHole(int[] mices, int[] holes) {
+    // code here
+    Arrays.sort(mices);
+    Arrays.sort(holes);
+
+    int res = 0;
+    for (int i = 0; i < mices.length; i++) {
+      res = Math.max(res, Math.abs(mices[i] - holes[i]));
+    }
+
+    return res;
+  }
+}
