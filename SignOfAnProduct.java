121b1d3a1b285b51d884dd8159f45c1a38eba32f
Aashi Jain <aashijain064@gmail.com>
2025-09-11 22:11:26 +0530

    Add files via upload

---
+++ SignOfAnProduct.java
@@ -0,0 +1,21 @@
+public class SignOfAnProduct {
+  // Implement a function signFunc(x) that returns:
+  // 1 if x is positive.
+  // -1 if x is negative.
+  // 0 if x is equal to 0.
+  // You are given an integer array nums. Let product be the product of all values
+  // in the array nums.
+
+  // Return signFunc(product).
+
+  public int arraySign(int[] nums) {
+    int negative = 0;
+    for (int i : nums) {
+      if (i == 0)
+        return 0;
+      if (i < 0)
+        negative++;
+    }
+    return negative % 2 == 0 ? 1 : -1;
+  }
+}
