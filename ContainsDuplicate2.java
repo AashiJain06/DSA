ca19dda95ab908389dbab04e8977a053e86e82a6
Aashi Jain <aashijain064@gmail.com>
2025-10-18 22:32:50 +0530

    Create ContainsDuplicate2.java

---
+++ ContainsDuplicate2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> hs = new HashMap<>();
+        for(int i =0;i<nums.length;i++)
+        {
+            if(hs.containsKey(nums[i]))
+            {
+                int idx = hs.get(nums[i]);
+                if((i-idx)<=k)
+                 return true;
+                else
+                hs.put(nums[i],i);
+            }
+            else
+             hs.put(nums[i],i);
+        }
+        return false;
+    }
+}ContainsDuplicate
