a741322e81a8d8e1176b8f71e6244e9876b46a86
Aashi Jain <aashijain064@gmail.com>
2025-11-28 21:58:47 +0530

    Create LeetCode112-PathSum.java

---
+++ LeetCode112-PathSum.java
@@ -0,0 +1,12 @@
+lass Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root==null)
+         return false;
+
+         targetSum-=root.val;
+        if(root.left==null && root.right==null)
+         return targetSum==0;
+
+        return hasPathSum(root.left,targetSum) || hasPathSum(root.right,targetSum);
+    }
+}
